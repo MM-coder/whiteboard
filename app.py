f7983e77b3c4e106f907cdf85744e87266b1ce0c
Nick Frost <nickfrostatx@gmail.com>
2016-04-15 13:48:47 -0700

    Actually write the file

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, Response, request, abort, render_template
+from werkzeug.security import safe_join
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('edit.html')
+
+@app.route('/<int:id>', methods=['GET', 'PUT'])
+def note(id):
+    if not path_for(id):
+        abort(404)
+
+    if request.method == 'GET':
+        return render_template('edit.html')
+
+    data = request.get_json()
+    if data is None or not isinstance(data, dict):
+        abort(400, 'Got %r for data' % data)
+    for k in ('title', 'text'):
+        if k not in data or not isinstance(data[k], type(u'')):
+            abort(400, '%s missing or bad type' % k)
+
+    writeNote(id, data['title'], data['text'])
+    return Response(status=204)
+
+
+def path_for(id):
+    path = safe_join('notes', str(id))
+    if path is None or not os.path.isfile(path):
+        return None
+    return path
+
+
+def writeNote(id, title, text):
+    with open(path_for(id), 'w') as f:
+        f.write(u'# {0}\n\n'.format(title))
+        f.write(text)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
