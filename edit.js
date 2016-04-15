22319eb27db5ef32881b10851e046c42e06d8eb9
Nick Frost <nickfrostatx@gmail.com>
2016-04-15 12:53:29 -0700

    Client side of autosave

--- edit.js
+++ edit.js
@@ -8,12 +8,41 @@
         + text
         + elem.value.substring(endPos);
       elem.selectionStart = elem.selectionEnd = startPos + text.length;
+      elem.focus();
+  };
+
+  var afterDelay = function() {
+    var timer = null;
+    return function(fn, delay) {
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(fn, delay);
+    };
+  };
+
+  var http = function(method, url, body) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = resolve;
+      xhr.onerror = reject;
+      xhr.send(body);
+    });
   };
 
   window.addEventListener('load', function() {
     var area = document.getElementsByTagName('textarea')[0];
 
+    var saveTimer = afterDelay();
     area.addEventListener('input', function() {
+      saveTimer(function() {
+        http('PUT', '/', area.value).then(function(e) {
+          console.log(e.target.response);
+        }, function(e) {
+          console.error(e);
+        });
+    }, 1000);
       this.setAttribute('rows', this.value.split("\n").length + 1 || 2);
     });
 
